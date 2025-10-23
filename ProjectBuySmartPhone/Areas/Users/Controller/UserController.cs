69c2bd201868ec3eea33f5e9f84f8f9e29cbf76b
hanbui3001 <bronzegoemo@gmail.com>
2025-10-23 23:11:13 +0700

    sua mau

---
+++ ProjectBuySmartPhone/Areas/Users/Controller/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectBuySmartPhone.Areas.Users.Controller
+{
+    public class UserController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IActionResult View()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
